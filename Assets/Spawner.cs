1897aa6d03cc5e91699fb25526a033b6f1c90822
Yequ Sun <yequsun@gmail.com>
2015-12-10 18:55:56 -0500

    Upload

---
+++ Assets/Spawner.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+	public GameObject[] groups;
+
+	// Use this for initialization
+	void Start () {
+		spawnNext ();
+	
+	}
+	
+	// Update is called once per frame
+	public void spawnNext() {
+		int i = Random.Range(0, groups.Length);
+
+		Instantiate(groups[i],
+		            transform.position,
+		            Quaternion.identity);
+	}
+}
